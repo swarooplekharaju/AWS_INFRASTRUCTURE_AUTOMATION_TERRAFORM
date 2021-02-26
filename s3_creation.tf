deefea55f32003f8f79ec558f878b4509f497b3b
Lekharaju sada siva Swaroop <swarooplekharaju@gmail.com>
2021-02-26 21:25:20 +0530

    aws s3 with terraform

--- s3_creation.tf
+++ s3_creation.tf
@@ -1,13 +1,22 @@
 /*
 store your aws credentials as config in .aws folder inside terraform directory 
 use profile default which tells terraform to locate credentials in .aws 
+
+install aws cli in windows/linux 
+type aws commands as well to check and practice the aws cli
+aws cli for checking s3 list is aws s3 ls 
+
 */
 
 
-provider "aws" { 
-  profile = "default"
-  region = "us-west-2"
+provider "aws"{
+region = "us-east-2"
+shared_credentials_file="C:\\Users\\terraform\\.aws\\creds"
+profile = "default"
+
 }
+
+
 
 resource "aws_s3_bucket" "s3_example" {
     bucket = "tf-practice-s3"
